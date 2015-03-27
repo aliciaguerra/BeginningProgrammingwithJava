357a16aefa9260c983d34a9ef71d606f0b95f289
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-27 01:17:51 -0700

    Create SnitSoftNew.java

---
+++ src/beginningprogrammingwithjava/SnitSoftNew.java
@@ -0,0 +1,15 @@
+class SnitSoftNew{
+public static void main(String args[]){
+double cdPrice;
+double shippingAndHandling;
+double total;
+
+cdPrice=5.95;
+shippingAndHandling=25.00;
+total=cdPrice+shippingAndHandling;
+
+System.out.print("We will bill $");
+System.out.println(total);
+System.out.println("to your credit card.");
+}
+}
