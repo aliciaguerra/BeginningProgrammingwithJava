c4346996dc763530f8401b051f08a0a7857da3e4
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-27 02:16:19 -0700

    Create CelsiustoFarenheit.java

---
+++ src/beginningprogrammingwithjava/CelsiustoFarenheit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+class CelsiusToFareheit{
+public static void main(String args[]){
+Scanner myScanner-new Scanner(System.in);
+double celsius, fahrenheit;
+System.out.print("Enter the Celsius temperature:");
+celsius=myScanner.nextDouble();
+
+fahrenheit=9.0/5.0 * celsius + 32.0;
+
+System.out.print("Room temperature?");
+System.out.println("farenheit==69.8");
+}
+}
