2b1762898dc5c023acb08c35c097f6998f85570c
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-27 01:05:12 -0700

    Create EchoLine2.java

---
+++ src/beginningprogrammingwithjava/EchoLine2.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+public class EchoLine2{
+public static void main(String args[]){
+throws IOException{
+InputStreamReader isr=new InputStreamReader(System.in);
+BufferedReader br=new BufferedReader(isr);
+String input=br.readLine();
+System.out.println(input);
+}
+}
