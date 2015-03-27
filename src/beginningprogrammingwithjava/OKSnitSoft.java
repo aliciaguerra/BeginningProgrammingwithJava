823435381a605de1678c6c198f4b9f167479724e
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-27 01:14:32 -0700

    Create OKSnitSoft.java

---
+++ src/beginningprogrammingwithjava/OKSnitSoft.java
@@ -0,0 +1,12 @@
+//this is okay
+class SnitSoft{
+static double amount=5.95;
+
+public static void main(String args[]){
+amount=amount+25.00;
+
+System.out.print("We will bill $");
+System.out.print("amount");
+System.out.println("to your credit card.");
+}
+}
