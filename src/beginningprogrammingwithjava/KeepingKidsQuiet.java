5c1a7acdc9c92bbca70ee7642745efacd64f6fce
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-27 01:20:53 -0700

    Create KeepingKidsQuiet.java

---
+++ src/beginningprogrammingwithjava/KeepingKidsQuiet.java
@@ -0,0 +1,15 @@
+class KeepingKidsQuiet{
+public static void main(String args[]){
+int gumballs;
+int kids;
+int gumballsPerKid;
+
+gumballs=30;
+kids=4;
+gumballsPerKid=gumball/kids;
+
+System.out.print("Each kid gets");
+System.out.print(gumballsPerKid);
+System.out.println("gumballs.");
+}
+}
