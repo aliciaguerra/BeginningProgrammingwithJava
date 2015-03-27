c4adc828ba53fe5167da660ba89bc84beb58fc5d
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-27 01:01:08 -0700

    Create EchoLine.java

---
+++ src/beginningprogrammingwithjava/EchoLine.java
@@ -0,0 +1,11 @@
+/*
+A program that lets you type one line of characters in the keyboard. As soon as you press enter, the program
+displays a second line that copies whatever you typed.
+*/
+import java.util.Scanner;
+class Echoline{
+public static main void(String args[]){
+Scanner=myScanner(system.in);
+System.out.println(myScanner.nextLine());
+}
+}
