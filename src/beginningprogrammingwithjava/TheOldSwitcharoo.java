f18226013cd953e19a919e2ee81c6d792410b1d1
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-28 01:31:16 -0700

    Create TheOldSwitcharoo.java

---
+++ src/beginningprogrammingwithjava/TheOldSwitcharoo.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+import java.util.Random;
+import static java.lang.System.out;
+
+class TheOldSwitceroo{
+public staic void main(String args[]){
+Scanner myScanner=new Scanner(System.in);
+Random myRandom = new Random();
+int randomNumber;
