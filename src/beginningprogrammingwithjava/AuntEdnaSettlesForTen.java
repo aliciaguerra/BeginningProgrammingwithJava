1e7ae3b38b4a7450f1cb41b4532a371c2bcdcc08
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-28 22:18:34 -0700

    Create AuntEdnaSettlesForTen.java

---
+++ src/beginningprogrammingwithjava/AuntEdnaSettlesForTen.java
@@ -0,0 +1,12 @@
+import static java.lang.System.out;
+class AuntEdnaSettlesForTen{
+public static void main(String args[]){
+for(int count=0;count<10;count++){
+out.print("I've chewed");
+out.print(count);
+out.print("time(s).");
+}
+out.println("Ten times! Hooray!");
+out.println("I can swallow!");
+}
+}
