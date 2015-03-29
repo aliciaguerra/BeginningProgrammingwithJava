5c9b02a7fd3a927f97d058f6ddcad589d339b4df
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-28 22:48:14 -0700

    Create ListCombinations.java

---
+++ src/beginningprogrammingwithjava/ListCombinations.java
@@ -0,0 +1,19 @@
+import static java.lang.System.out;
+class ListCombinations{
+enum Symbol{
+cherry, lemon, kumquat,rutabage
+}
+public static void main(String args[]){
+ for(Symbol leftReel:Symbol.values()){
+  for(Symbol middleReel: Symbol.values()){
+   for(Symbol rightReel: Symbol.values()){
+out.print(leftReel);
+out.print("");
+out.print(middleReel);
+out.print("");
+out.println(rightReel);
+}
+}
+}
+}
+}
