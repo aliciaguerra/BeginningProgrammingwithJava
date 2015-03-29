236cff9ea8afc11a2ffe538a6b83cb4b10d1912f
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-29 13:50:08 -0700

    Update VacanciesInReverse.java

--- src/beginningprogrammingwithjava/VacanciesInReverse.java
+++ src/beginningprogrammingwithjava/VacanciesInReverse.java
@@ -4,3 +4,20 @@ import java.io.FileNotFoundException;
 
 class VacanciesInReverse{
 public static void main(String args[])throws FileNotFoundException{
+  Scanner diskScanner=new Scanner(new File("occupancy"));
+ int guestsIn[];
+ guestsIn=new int[10];
+ 
+for(int roomNum=0; roomNum<10; roomNum++){
+  guestsIn[roomNum] = diskScanner.nextInt();
+}
+
+for(int roomNum=9; roomNum>=0; roomNum--){
+  if(guestsIn[roomNum] == 0){
+    System.out.print("Room");
+    System.out.print(roomNum);
+    System.out.println("is Vacant.");
+  }
+}
+}
+}
