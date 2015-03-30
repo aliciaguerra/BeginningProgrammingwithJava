ace37a837edd5d087e03a352a26d2d884a1a2a84
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-29 18:44:28 -0700

    Create variableinsidemainmethod.java

---
+++ src/beginningprogrammingwithjava/variableinsidemainmethod.java
@@ -0,0 +1,8 @@
+//i was too lazy to write out the entire code and format it :P
+class variableinsidemainmethod{
+public static void main(String args[]){
+double amount=5.95;
+amount=amount+25.00;
+System.out.println(amount);
+}
+}
