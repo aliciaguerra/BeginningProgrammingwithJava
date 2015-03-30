35a57d940330f27e9a536063b9e3bac3e1386ba3
Alicia Guerra <aiguerra@ucsd.edu>
2015-03-29 17:54:50 -0700

    Create JazzyEchoLine.java

---
+++ src/beginningprogrammingwithjava/JazzyEchoLine.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class JazzyEchoLine{
+public static void main(String args[]){
+Scanner myScanner=new Scanner(System.in);
+String lineIn;
+
+lineIn=myScanner.nextLine();
+System.out.println(lineIn);
+}
+}
